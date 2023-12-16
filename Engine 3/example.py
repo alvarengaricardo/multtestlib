791b626466aca39b438fcdd0e6089269efe537ff
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-12-16 20:16:30 -0300

    new engine

--- Engine 3/example.py
+++ Engine 3/example.py
@@ -1,13 +1,3 @@
- # multtestlib
-
-**multtestlib** is a library for running unit tests using processing parallel, written in Python.
-
-![table](https://user-images.githubusercontent.com/16055876/222324613-78df26fa-1cf5-48ce-b8e8-4018d6a2224c.png)
-
-## Example
-
-**Code:**
-
 import multtestlib as mtl
 import library_to_test as ltt
 
@@ -32,10 +22,3 @@ if __name__ == "__main__":
     mtl.init()
     main()
     mtl.end()
-
-
-**Generated Files**
-
-    filepass.txt -> Contains passed tests
-    filefail.txt -> Contains failed tests
-    filetot.txt -> Contains all tests
